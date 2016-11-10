1bb1ecaefac0fcdd5377926c90cc9d6e6b59000c
Peter Salanki <peter@salanki.st>
2016-11-10 00:36:06 -0500

    Modernize and add existance check
    There is no separate image cache anymore, removed the calls for it. RN now supports turning native calls to promises directly, removed boilerplate. Added method to check if a URL exits in the cache.

--- ios/RCTHttpCache/RCTHttpCache.m
+++ ios/RCTHttpCache/RCTHttpCache.m
@@ -7,70 +7,44 @@
 //
 
 #import "RCTHttpCache.h"
-#import "RCTImageLoader.h"
 #import "RCTBridge.h"
 
 @implementation RCTHttpCache
 
-@synthesize bridge = _bridge;
+//@synthesize bridge = _bridge;
 
 RCT_EXPORT_MODULE(HttpCache);
 
-RCT_EXPORT_METHOD(getHttpCacheSize:(RCTResponseSenderBlock)resolve)
+RCT_REMAP_METHOD(existsInCache,
+                 url:(NSString *)url
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
 {
-    NSURLCache *httpCache = [NSURLCache sharedURLCache];
-    resolve(@[[NSNull null], @([httpCache currentDiskUsage])]);
+  NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+  NSURLCache *httpCache = [NSURLCache sharedURLCache];
+  
+  NSCachedURLResponse *response = [httpCache cachedResponseForRequest: request];
+  BOOL inCache = response != nil;
+  
+  resolve(@(inCache));
 }
 
-RCT_EXPORT_METHOD(clearCache:(RCTResponseSenderBlock)resolve)
+
+RCT_REMAP_METHOD(getHttpCacheSize,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    NSURLCache *httpCache = [NSURLCache sharedURLCache];
+    resolve(@([httpCache currentDiskUsage]));
+}
+
+RCT_REMAP_METHOD(clearCache,
+                 resolverz:(RCTPromiseResolveBlock)resolve
+                 rejecterz:(RCTPromiseRejectBlock)reject)
 {
     NSURLCache *httpCache = [NSURLCache sharedURLCache];
     [httpCache removeAllCachedResponses];
     resolve(@[[NSNull null]]);
 }
-
-
-RCT_EXPORT_METHOD(getImageCacheSize:(RCTResponseSenderBlock)resolve)
-{
-    NSURLCache *imageCache = [self imageCache];
-    dispatch_queue_t queue = [self imageCacheQueue];
-    if (imageCache == nil || queue == nil) {
-        resolve(@[@"cache not found"]);
-    }
-    dispatch_async(queue, ^{
-        resolve(@[[NSNull null], @([imageCache currentDiskUsage])]);
-    });
-}
-
-RCT_EXPORT_METHOD(clearImageCache:(RCTResponseSenderBlock)resolve)
-{
-    NSURLCache *imageCache = [self imageCache];
-    dispatch_queue_t queue = [self imageCacheQueue];
- 
-    if (imageCache == nil || queue == nil) {
-        resolve(@[@"cache not found"]);
-    }
-
-    dispatch_async(queue, ^{
-        [imageCache removeAllCachedResponses];
-        resolve(@[[NSNull null]]);
-    });
-}
-
-- (NSURLCache *)imageCache
-{
-    RCTImageLoader* loader = _bridge.imageLoader;
-    NSURLCache *cache = [loader valueForKey:@"_URLCache"];
-    
-    return cache;
-}
-
-- (dispatch_queue_t)imageCacheQueue
-{
-    RCTImageLoader* loader = _bridge.imageLoader;
-    dispatch_queue_t queue = [loader valueForKey:@"_URLCacheQueue"];
-    return queue;
-}
-
 
 @end
